2a33764d21b8a2175eb644ccc584d342b68ab488
twtzptr <twtzzz@yandex.ru>
2022-12-08 21:28:31 +0300

    .

--- separated_rights.py
+++ separated_rights.py
@@ -4,7 +4,7 @@
 from enum import Enum
 import random
 
-USERS = ['Boris', 'Victor', 'Konstantin', 'Evgeny']
+USERS = [('Boris', '1234'), ('Victor', '1234'), ('Konstantin', '1234'), ('Evgeny', '1234')]
 OBJECTS = ['FILE 1', 'DATABASE', 'FILE 2', 'CLOUD DATABASE', 'CLOUD SERVER INSTANCE', 'CD-RW']
 AccessTypes = Enum('AccessTypes', ['READ', 'WRITE', 'TRANSFER', 'FULL'])
 
@@ -32,7 +32,7 @@ class AccessRights:
         userIndex = 0
 
         for user_rights in self.access_matrix:
-            rightsStr = "{:^20}".format(USERS[userIndex])
+            rightsStr = "{:^20}".format(USERS[userIndex][0])
             for right in user_rights:
                 rightsStr += "{:^20}".format(right.name)
             
@@ -42,6 +42,43 @@ class AccessRights:
 
         return res
 
+    def try_login(self, userIndex, password):
+        if userIndex >= len(USERS):
+            return None
 
-a = AccessRights()
-print(a)
+        (username, true_password) = USERS[userIndex]
+
+        if (true_password == password):
+            return username
+
+        return None
+
+def handle_user_action(userIndex):
+    print('Введите действие, которое необходимо выполнить:')
+    print('1 - чтение')
+    print('2 - запись')
+    print('3 - передача прав')
+    user_action = int(input(userIndex))
+
+    if (user_action > 3 or user_action < 0):
+        print('Неверное действие')
+        return
+    
+
+access_rights = AccessRights()
+
+exit = False
+
+while (not exit):
+    print(str(access_rights))
+    print('Введите индекс пользователя, за которого необходимо войти')
+    userIndex = int(input())
+    print('Введите пароль')
+    password = input()
+    print()
+    username = access_rights.try_login(userIndex, password)
+    
+    if (username):
+        handle_user_action(userIndex)
+    else:
+        print('Неверный индекс пользователя/пароль')
